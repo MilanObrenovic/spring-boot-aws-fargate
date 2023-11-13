3f690c113534a9acab3cc8dc3b858e48a41930c2
Milan Obrenovic <mmobrenovic@gmail.com>
2023-11-13 22:25:09 +0100

    Added ping pong controller

--- src/main/java/com/example/notes/controller/PingPongController.java
+++ src/main/java/com/example/notes/controller/PingPongController.java
@@ -1,78 +1,22 @@
 package com.example.notes.controller;
 
-import com.example.notes.dto.NoteDTO;
-import com.example.notes.dto.request.NoteCreateRequestDTO;
-import com.example.notes.dto.request.NoteUpdateRequestDTO;
-import com.example.notes.service.NoteService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = "/api/v1/notes")
-public class NoteController {
+@RequestMapping(value = "/api/v1/ping")
+public class PingPongController {
 
-    private final NoteService noteService;
-
-    @PostMapping()
-    public ResponseEntity<NoteDTO> createNote(
-            @RequestBody NoteCreateRequestDTO requestDTO
-    ) {
-        NoteDTO noteDTO = noteService.createNote(requestDTO);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(noteDTO);
-    }
-
-    @GetMapping("/{noteId}")
-    public ResponseEntity<NoteDTO> findNoteById(
-            @PathVariable("noteId") Long noteId
-    ) {
-        NoteDTO noteDTO = noteService.findNoteById(noteId);
+    @GetMapping("/pong")
+    public ResponseEntity<String> findPong() {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(noteDTO);
-    }
-
-    @GetMapping()
-    public ResponseEntity<List<NoteDTO>> findPaginatedNotes(
-            Pageable pageable
-    ) {
-        List<NoteDTO> paginatedNoteDTOs = noteService.findPaginatedNotes(pageable);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(paginatedNoteDTOs);
-    }
-
-    @PutMapping("/{noteId}")
-    public ResponseEntity<NoteDTO> updateNoteById(
-            @PathVariable("noteId") Long noteId,
-            @RequestBody NoteUpdateRequestDTO requestDTO
-    ) {
-        NoteDTO updatedNoteDTO = noteService.updateNoteById(
-                noteId,
-                requestDTO
-        );
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(updatedNoteDTO);
-    }
-
-    @DeleteMapping("/{noteId}")
-    public ResponseEntity<Void> deleteNoteById(
-            @PathVariable("noteId") Long noteId
-    ) {
-        noteService.deleteNoteById(noteId);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .build();
+                .body("Pong!");
     }
 
 }
